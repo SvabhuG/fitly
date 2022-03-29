641b6fc14396f927fa874252e17fbd0ef4cfc2cf
jrtaubert <jtaubert@wpi.edu>
2022-03-29 10:31:53 -0400

    workouts tab!!

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
       ),
       home: Scaffold(
-          body:
-          child: Text ()Container(
-            height:100.0,
-              decoration: BoxDecoration(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text( "Workouts",
+              style: TextStyle(fontSize: 25),
+              ),
+
+              Container(
+                height:100.0,
+                width:300.0,
+                decoration: BoxDecoration(
                   color: Colors.red[300],
                   border: Border.all(),
-                  borderRadius: const BorderRadius.all(Radius.circular(50))),
+                  borderRadius: const BorderRadius.all(Radius.circular(20))),
               child: const Center(
                 child: Text("Log New Workout"),
-              ))),
+              ))]))),
     );
   }
 }
