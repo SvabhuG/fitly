c398839640284721b762858c89c85cf27284b851
jrtaubert <jtaubert@wpi.edu>
2022-03-30 11:15:33 -0400

    workouts tab!!

--- lib/main.dart
+++ lib/main.dart
@@ -10,55 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Workouts',
-      theme: ThemeData(),
-      home: Scaffold(
+        title: 'Workouts',
+        theme: ThemeData(),
+        home: Scaffold(
           body: Center(
               child: Column(
                   //mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-            const Text(
-              'Workouts',
-              style: TextStyle(fontSize: 25),
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.redAccent),
-                overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return Colors.blue;
-                    }
-                    return null;
-                  },
+                const Text(
+                  'Workouts',
+                  style: TextStyle(fontSize: 25),
                 ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                'Log New Workout',
-                style: TextStyle(fontSize: 15),
-              ),
-            ),
-            Wrap(spacing: 10.0,
-                children: [
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.redAccent),
+                    overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.greenAccent;
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Log New Workout',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Wrap(spacing: 5.0, children: [
                   Container(
-                      height: 100.0,
-                      width: 300.0,
+                      height: 50.0,
+                      width: 150.0,
                       decoration: BoxDecoration(
                           color: Colors.red[300],
                           border: Border.all(),
                           borderRadius:
-                          const BorderRadius.all(Radius.circular(20))),
+                              const BorderRadius.all(Radius.circular(20))),
                       child: const Center(
                         child: Text("Your Next Workout:"),
                       )),
                   Container(
                       child: const Center(
-                        child: Text('the date lol'),
-                      )),
-                ])
-          ]))),
-    );
+                    child: Text('the date lol'),
+                  )),
+                ]),
+                Container(
+                  height: 100.0,
+                  width: 300.0,
+                  decoration: BoxDecoration(
+                      color: Colors.red[300],
+                      border: Border.all(),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: Column(children: const [
+                    Text("jjgnhvhgt"),
+                    Text("klasfjhlaksdjh"),
+                  ]),
+                )
+              ])),
+        ));
   }
 }
