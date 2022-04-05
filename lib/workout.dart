4bf1625bd5fde4a2ed5e60314fce2c4b2603b31d
jrtaubert <jtaubert@wpi.edu>
2022-04-05 09:13:56 -0400

    new workout page!!!

---
+++ lib/workout.dart
@@ -0,0 +1,125 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WorkoutPage());
+}
+
+class WorkoutPage extends StatefulWidget {
+  const WorkoutPage({Key? key}) : super(key: key);
+
+  @override
+  State<WorkoutPage> createState() => _WorkoutPageState();
+}
+
+class _WorkoutPageState extends State<WorkoutPage> {
+  Color _bgrdcolor1 = const Color(0xff222222);
+  Color _iconcolor1 = Colors.purple;
+
+  Color _bgrdcolor2 = const Color(0xff222222);
+  Color _iconcolor2 = Colors.purple;
+
+  Color _bgrdcolor3 = const Color(0xff222222);
+  Color _iconcolor3 = Colors.purple;
+
+  static const TextStyle hintStyle =
+      TextStyle(color: Color(0xff4c4c58), fontSize: 10);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+   /*   theme: ThemeData(
+          textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: Colors.white,
+
+            displayColor: Colors.white,
+          ),
+      ),
+      */
+      home: Scaffold(
+        backgroundColor: Color(0xff000000),
+
+        appBar: AppBar(
+          title: const Text(
+            'Workouts',
+            style: TextStyle(fontSize: 30),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Container(
+                      margin: EdgeInsets.all(20),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.purple,
+                            onPrimary: Colors.white,
+                            shadowColor: Colors.greenAccent,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            minimumSize: const Size(200, 60), //////// HERE
+                          ),
+
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const WorkoutPage())
+                            );
+                          },
+                          child: const Text(
+                              'Next',
+                              style: TextStyle(
+                                  fontSize: 18
+                              )
+                          )
+                      )
+                  )
+                  ]
+              ),
+              Row(
+                children: [
+                  Container(
+                      height: 50.0,
+                      width: 150.0,
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                          color: Colors.red[300],
+                          border: Border.all(),
+                          borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                      child: const Center(
+                        child: Text("Your Next Workout:"),
+                      )),
+                  Container(
+                    height: 50.0,
+                    width: 25.0,
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red[300],
+                      border: Border.all(),
+                    ),
+                  ),
+                  Row(
+                    children: const [
+                      Text('the date lmao'),
+                      Icon(Icons.calendar_month_outlined),
+                    ],
+                  )
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
