a1f5aa565e069dd0e49d8518f21eb18b0b799a10
jrtaubert <jtaubert@wpi.edu>
2022-04-13 09:20:04 -0400

    added some *enhancements* to the code!!!!!!!!!!!!!

--- android/app/src/main/java/model/Exercise.java
+++ android/app/src/main/java/model/Exercise.java
@@ -11,58 +11,122 @@ public class Exercise {
 	private String muscleGroup;
 
 //constructor
-
-	public Exercise(String name, String muscle, double userMaxWeight, int userReps, int userSets, int userIntensity) {
+	/**
+	 * Constructs a new exercise object with a given name, muscle group, weight
+	 * (that they last used), last reps, last sets, and intensity of that exercise.
+	 * 
+	 * @param name          name of the exercise
+	 * @param muscle        name of the muscle group
+	 * @param userWeight    last used weight
+	 * @param userReps      last number of reps
+	 * @param userSets      last number of sets
+	 * @param userIntensity user-opinion of intensity of the exercise
+	 */
+	public Exercise(String name, String muscle, double userWeight, int userReps, int userSets, int userIntensity) {
 		exerciseName = name;
 		muscleGroup = muscle;
-		weight = userMaxWeight;
-		//weight = Math.round(userMaxWeight * .85 / 5) * 5;
+		weight = userWeight;
+		// weight = Math.round(userMaxWeight * .85 / 5) * 5;
 		sets = userSets;
 		reps = userReps;
 		intensity = userIntensity;
 
 	}
 
+	/**
+	 * returns the muscle group
+	 * 
+	 * @return the muscle group
+	 */
 	public String getMuscleGroup() {
 		return muscleGroup;
 	}
 
+	/**
+	 * returns the name of the exercise
+	 * 
+	 * @return name of exercise
+	 */
 	public String getName() {
 		return exerciseName;
 	}
 
+	/**
+	 * formats the sets, reps, name, and weight of the exercise as a readable
+	 * exercise
+	 */
 	public String toString() {
 		return getSets() + " x " + getReps() + " " + getName() + " " + getWeight() + " lbs";
 	}
 
-	int getIntensity() {
+	/**
+	 * returns the intensity of the exercise
+	 * 
+	 * @return intensity
+	 */
+	public int getIntensity() {
 		return intensity;
 	}
 
+	/**
+	 * sets the intensity to a given intensity
+	 * 
+	 * @param intense the new intensity
+	 */
 	private void setIntensity(int intense) {
 		intensity = intense;
 	}
 
+	/**
+	 * returns the current weight that the user is lifting
+	 * 
+	 * @return the weight
+	 */
 	public double getWeight() {
 		return weight;
 	}
 
+	/**
+	 * sets the weight to a given weight
+	 * 
+	 * @param weight the new weight
+	 */
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
 
+	/**
+	 * returns the current sets the user is doing
+	 * 
+	 * @return the number of sets
+	 */
 	public int getSets() {
 		return sets;
 	}
 
+	/**
+	 * changes the number of sets to a given number of sets
+	 * 
+	 * @param sets new number of sets
+	 */
 	public void setSets(int sets) {
 		this.sets = sets;
 	}
 
+	/**
+	 * returns the number of reps
+	 * 
+	 * @return number of reps
+	 */
 	public int getReps() {
 		return reps;
 	}
 
+	/**
+	 * changes the number of reps to a given number of reps
+	 * 
+	 * @param reps the new number of reps
+	 */
 	public void setReps(int reps) {
 		this.reps = reps;
 	}
--- android/app/src/main/java/model/WorkoutGenerator.java
+++ android/app/src/main/java/model/WorkoutGenerator.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WorkoutGenerator {
 
@@ -12,8 +13,18 @@ public class WorkoutGenerator {
 		Exercise dbrows = new Exercise("Rows", "Back", 15, 5, 12, 5);
 		Exercise latpulldowns = new Exercise("Lat Pull Downs", "Back", 45, 6, 10, 2);
 
-		
-		
+		Scanner sc = new Scanner(System.in);
+
+		// right now only Chest or Back
+		// also user would select a box, not text input (so less chance of them typing
+		// in something wrong)
+		System.out.println("What muscle group would you like to work out?");
+		String workout = sc.next();
+		System.out.println("Would you like to workout another muscle group? If yes, which one?");
+		String workout2 = sc.next();
+
+		sc.close();
+
 		ArrayList<Exercise> Exercises = new ArrayList<Exercise>();
 		Exercises.add(flyes);
 		Exercises.add(benchPress);
@@ -22,45 +33,49 @@ public class WorkoutGenerator {
 		Exercises.add(dbrows);
 		Exercises.add(latpulldowns);
 
-
 		// set inputs to what they actually did not what we suggested
-		// if weight increase isn't a multiple of 5, add more reps?
-		// if statements for each muscle group?
-		// running into issues with increasing weight
+		// if statements for each muscle group? (if they only want chest/back, weed out
+		// others?)
+		// running into issues with increasing weight?
 
 		// intensity scaling
 		int count = 0;
 
 		for (Exercise exercise : Exercises) {
-			if (count % 3 == 0)
-				System.out.println("\n" + exercise.getMuscleGroup() + " Workout");
-			count++;
+			if (exercise.getMuscleGroup().equalsIgnoreCase(workout)
+					|| exercise.getMuscleGroup().equalsIgnoreCase(workout2))
 
-			if (exercise.getIntensity() >= 8) {
-				exercise.setReps(exercise.getReps() - 2);
-				exercise.setWeight(Math.round((exercise.getWeight() * .85) / 5) * 5);
+			{
 
-				/*
-				 * if (exercise.getWeight() * .85 % 5 != 0) exercise.setReps(exercise.getReps()
-				 * + 1); else exercise.setWeight(exercise.getWeight() * .85);
-				 */
-				// sets stays the same?
-			} else if (exercise.getIntensity() > 4 && exercise.getIntensity() < 8) {
-				exercise.setReps(exercise.getReps() + 1);
-				/*
-				 * if (exercise.getWeight() * 1.05 % 5 != 0) exercise.setReps(exercise.getReps()
-				 * + 1); else exercise.setWeight(exercise.getWeight() * 1.05);
-				 */
+				if (count % 3 == 0)
+					System.out.println("\n" + exercise.getMuscleGroup() + " Workout");
+				count++;
 
-//sets and reps stay the same?
-			} else if (exercise.getIntensity() <= 4) {
-				exercise.setWeight(Math.round((exercise.getWeight() * 1.05) / 5) * 5);
-				exercise.setSets(exercise.getSets() + 1);
-			}
-			System.out.println(exercise.toString());
+				if (exercise.getIntensity() >= 8) {
+					exercise.setReps(exercise.getReps() - 2);
+					exercise.setWeight(Math.round((exercise.getWeight() * .85) / 5) * 5);
 
+					/*
+					 * if (exercise.getWeight() * .85 % 5 != 0) exercise.setReps(exercise.getReps()
+					 * + 1); else exercise.setWeight(exercise.getWeight() * .85);
+					 */
+// sets stays the same?
+				} else if (exercise.getIntensity() > 4 && exercise.getIntensity() < 8) {
+					exercise.setReps(exercise.getReps() + 1);
+					/*
+					 * if (exercise.getWeight() * 1.05 % 5 != 0) exercise.setReps(exercise.getReps()
+					 * + 1); else exercise.setWeight(exercise.getWeight() * 1.05);
+					 */
+
+//sets and weight stay the same?
+				} else if (exercise.getIntensity() <= 4) {
+					exercise.setWeight(Math.round((exercise.getWeight() * 1.05) / 5) * 5);
+					exercise.setSets(exercise.getSets() + 1);
+// reps stay the same?
+				}
+				System.out.println(exercise.toString());
+			} // end if
 		} // end for
-
 
 		// time since last scaling
 		/**
