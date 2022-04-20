324b11f89092adf7a0b817ceeeaa722dc361f823
Svabhu Govindaraj <svabhug@gmail.com>
2022-04-20 08:45:55 -0400

    added multi-select on muscles page

--- lib/muscles.dart
+++ lib/muscles.dart
@@ -15,21 +15,18 @@ class MusclesPage extends StatefulWidget {
 
 class _MusclesPageState extends State<MusclesPage> {
 
-  Color _bgrdcolor1 = const Color(0xff222222);
-  Color _iconcolor1 = Colors.purple;
-
-  Color _bgrdcolor2 = const Color(0xff222222);
-  Color _iconcolor2 = Colors.purple;
-
-  Color _bgrdcolor3 = const Color(0xff222222);
-  Color _iconcolor3 = Colors.purple;
+  var bgrdcolors = List.generate(6, (_) => Color(0xff222222));
 
   static const TextStyle hintStyle = TextStyle(color: Color(0xff4c4c58), fontSize: 10);
 
   @override
   Widget build(BuildContext context) {
+
+
     double unitHeightValue = MediaQuery.of(context).size.width * 0.001;
     double iconSize = unitHeightValue*100;
+    var muscles = <String>[];
+
     TextStyle buttonStyle = TextStyle(fontSize: unitHeightValue*40);
     TextStyle questStyle = TextStyle(fontSize: unitHeightValue*40);
     return Scaffold(
@@ -58,7 +55,7 @@ class _MusclesPageState extends State<MusclesPage> {
                       margin: EdgeInsets.symmetric(vertical: 20),
                       child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                            primary: _bgrdcolor3,
+                            primary: bgrdcolors[0],
                             elevation: 0,
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(32.0)),
@@ -67,14 +64,14 @@ class _MusclesPageState extends State<MusclesPage> {
 
                           onPressed: () {
                             setState(() {
-                              _bgrdcolor3 = Colors.purple;
-                              _iconcolor3 = Colors.white;
+                                bgrdcolors[0] = (bgrdcolors[0] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
+                                if(muscles.contains("Chest")){
+                                  muscles.remove("Chest");
+                                }
+                                else{
+                                  muscles.add("Chest");
+                                }
 
-                              _bgrdcolor2 = const Color(0xff222222);
-                              _iconcolor2 = Colors.purple;
-
-                              _bgrdcolor1 = const Color(0xff222222);
-                              _iconcolor1 = Colors.purple;
                             });
                           },
                           child: Padding(
@@ -87,28 +84,28 @@ class _MusclesPageState extends State<MusclesPage> {
                       )
                     ),
                     Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
+                      margin: const EdgeInsets.symmetric(vertical: 20),
                       child: ElevatedButton(
+
                           style: ElevatedButton.styleFrom(
-                            primary: _bgrdcolor3,
+                            primary: bgrdcolors[1],
                             elevation: 0,
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(32.0)),
                             minimumSize: const Size(240, 60), //////// HERE
                           ),
 
-                          onPressed: () {
-                            setState(() {
-                              _bgrdcolor3 = Colors.purple;
-                              _iconcolor3 = Colors.white;
-
-                              _bgrdcolor2 = const Color(0xff222222);
-                              _iconcolor2 = Colors.purple;
-
-                              _bgrdcolor1 = const Color(0xff222222);
-                              _iconcolor1 = Colors.purple;
-                            });
-                          },
+                        onPressed: () {
+                          setState(() {
+                            bgrdcolors[1] = (bgrdcolors[1] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
+                          });
+                          if(muscles.contains("Biceps")){
+                            muscles.remove("Biceps");
+                          }
+                          else{
+                            muscles.add("Biceps");
+                          }
+                        },
                           child: Padding(
                             padding: const EdgeInsets.symmetric(vertical: 30),
                             child: Text(
@@ -118,136 +115,189 @@ class _MusclesPageState extends State<MusclesPage> {
                           ),
                       ),
                     ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: _bgrdcolor3,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0)),
-                          minimumSize: const Size(240, 60), //////// HERE
-                        ),
+                    Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: bgrdcolors[2],
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            minimumSize: const Size(240, 60), //////// HERE
+                          ),
 
                         onPressed: () {
                           setState(() {
-                            _bgrdcolor3 = Colors.purple;
-                            _iconcolor3 = Colors.white;
-
-                            _bgrdcolor2 = const Color(0xff222222);
-                            _iconcolor2 = Colors.purple;
-
-                            _bgrdcolor1 = const Color(0xff222222);
-                            _iconcolor1 = Colors.purple;
+                            bgrdcolors[2] = (bgrdcolors[2] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
                           });
+                          if(muscles.contains("Legs")){
+                            muscles.remove("Legs");
+                          }
+                          else{
+                            muscles.add("Legs");
+                          }
                         },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 30),
-                          child: Text(
-                              'Legs',
-                              style: buttonStyle
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30),
+                            child: Text(
+                                'Legs',
+                                style: buttonStyle
+                            ),
                           ),
-                        ),
+                      )
                     )
                   ]
                 ),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: _bgrdcolor3,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0)),
-                          minimumSize: const Size(240, 60), //////// HERE
-                        ),
-
-                        onPressed: () {
-                          setState(() {
-                            _bgrdcolor3 = Colors.purple;
-                            _iconcolor3 = Colors.white;
-
-                            _bgrdcolor2 = const Color(0xff222222);
-                            _iconcolor2 = Colors.purple;
-
-                            _bgrdcolor1 = const Color(0xff222222);
-                            _iconcolor1 = Colors.purple;
-                          });
-                        },
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children:[
-                              Padding(
-                                padding: EdgeInsets.symmetric(vertical: 30),
-                                child: Text(
-                                    'Back',
-                                    style: buttonStyle
-                                ),
-                              ),
-                            ]
-                        )
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: _bgrdcolor3,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0)),
-                          minimumSize: const Size(240, 60), //////// HERE
-                        ),
-
-                        onPressed: () {
-                          setState(() {
-                            _bgrdcolor3 = Colors.purple;
-                            _iconcolor3 = Colors.white;
-
-                            _bgrdcolor2 = const Color(0xff222222);
-                            _iconcolor2 = Colors.purple;
-
-                            _bgrdcolor1 = const Color(0xff222222);
-                            _iconcolor1 = Colors.purple;
-                          });
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 30),
-                          child: Text(
-                              'Triceps',
-                              style: buttonStyle
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: bgrdcolors[3],
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            minimumSize: const Size(240, 60), //////// HERE
                           ),
-                        ),
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: _bgrdcolor3,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32.0)),
-                          minimumSize: const Size(240, 60), //////// HERE
-                        ),
 
                         onPressed: () {
                           setState(() {
-                            _bgrdcolor3 = Colors.purple;
-                            _iconcolor3 = Colors.white;
-
-                            _bgrdcolor2 = const Color(0xff222222);
-                            _iconcolor2 = Colors.purple;
-
-                            _bgrdcolor1 = const Color(0xff222222);
-                            _iconcolor1 = Colors.purple;
+                            bgrdcolors[3] = (bgrdcolors[3] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
                           });
+                          if(muscles.contains("Back")){
+                            muscles.remove("Back");
+                          }
+                          else{
+                            muscles.add("Back");
+                          }
                         },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 30),
-                          child: Text(
-                              'Shoulders',
-                              style: buttonStyle
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 30),
+                            child: Text(
+                                'Back',
+                                style: buttonStyle
+                            ),
                           ),
-                        ),
+                      ),
+                    ),
+                    Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: bgrdcolors[4],
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            minimumSize: const Size(240, 60), //////// HERE
+                          ),
+
+                        onPressed: () {
+                          setState(() {
+                            bgrdcolors[4] = (bgrdcolors[4] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
+                          });
+                          if(muscles.contains("Triceps")){
+                            muscles.remove("Triceps");
+                          }
+                          else{
+                            muscles.add("Triceps");
+                          }
+                        },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30),
+                            child: Text(
+                                'Triceps',
+                                style: buttonStyle
+                            ),
+                          ),
+                      ),
+                    ),
+                    Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: bgrdcolors[5],
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
+                            minimumSize: const Size(240, 60), //////// HERE
+                          ),
+
+                        onPressed: () {
+                          setState(() {
+                            bgrdcolors[5] = (bgrdcolors[5] == Color(0xff222222)) ? Colors.purple:Color(0xff222222);
+                          });
+                          if(muscles.contains("Shoulders")){
+                            muscles.remove("Shoulders");
+                          }
+                          else{
+                            muscles.add("Shoulders");
+                          }
+                        },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30),
+                            child: Text(
+                                'Shoulders',
+                                style: buttonStyle
+                            ),
+                          ),
+                      )
                     )
                   ]
                 )
               ],
-            )
+            ),
+            Container(
+                margin: const EdgeInsets.all(20.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Expanded(
+                      child: LinearPercentIndicator(
+                        width: MediaQuery.of(context).size.width - 50,
+                        animation: true,
+                        lineHeight: 10.0,
+                        animationDuration: 500,
+                        percent: 0.33,
+                        linearStrokeCap: LinearStrokeCap.roundAll,
+                        backgroundColor: Colors.purple[200],
+                        progressColor: Colors.purple,
+                      ),
+                    )
+                    ]
+                )
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Container(
+                    margin: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.purple,
+                          onPrimary: Colors.white,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32.0)),
+                          minimumSize: const Size(200, 60), //////// HERE
+                        ),
+
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => InfoPage())
+                          );
+                        },
+                        child: const Text(
+                            'Next',
+                            style: TextStyle(
+                                fontSize: 18
+                            )
+                        )
+                    )
+                )
+                ]
+            ),
           ],
         )
       )
