ac9b04c39ee5a46b25725c2940a577c02187e5cf
jrtaubert <jtaubert@wpi.edu>
2022-05-13 12:04:50 -0400

    pleast -> please

--- lib/gender.dart
+++ lib/gender.dart
@@ -53,7 +53,7 @@ class _GenderPageState extends State<GenderPage> {
               Container(
                 margin: const EdgeInsets.only(top: 40),
                   child: const Text(
-                      'Pleast select your gender',
+                      'Please select your gender',
                       style: TextStyle(fontSize: 24)
                   )
               ),
